d236afc8db0365cb869766c4983b346fb66ed979
ghazalb76 <ghazal.b93@gmail.com>
2019-05-30 19:52:17 +0430

    Add model for users

--- mysite/signUp/models.py
+++ mysite/signUp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length = 200)
+    last_name = models.CharField(max_length = 200)
+    user_name = models.CharField(max_length = 200, default = 'old_user')
+    email = models.EmailField()
+    password =  models.CharField(max_length = 200)
+
+    def __str__(self):
+        return self.email
+
